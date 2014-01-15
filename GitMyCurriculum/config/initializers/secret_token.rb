bbf9ebb7f7cb4fc357024d52af6ab2b789943899
bammons <baileyammons@gmail.com>
2014-01-15 13:35:23 -0700

    Pushed initial set up and dependancies.

---
+++ GitMyCurriculum/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitMyCurriculum::Application.config.secret_key_base = 'c281314b1ea52e88bad2af9b8f4eb9e4197f2508e0a69118c18ddc58a3bdd6930a7a80d1a7da92b720ddf886800d973782e5d0170cc872b054045430247ab8d1'
