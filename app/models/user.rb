cbf1838cde259821c506aaec98ea8260006a38fa
bammons <baileyammons@gmail.com>
2014-02-01 13:16:27 -0700

    User signin with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+   # Include default devise modules. Others available are:
+   # :confirmable, :lockable, :timeoutable and :omniauthable
+   devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :trackable, :validatable
+
+   attr_accessor :login
+
+   def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
+
+   validates :username,
+      :uniqueness => {
+         :case_sensitive => false
+      }
 end
