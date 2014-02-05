d5a56fcbbbcc0f318ede4cbc02f6fdb6efedc2b7
bammons <baileyammons@gmail.com>
2014-02-04 17:31:07 -0700

    initial test creation

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe SearchController do
 
-  describe "GET 'user_search'" do
-    it "returns http success" do
-      get 'user_search'
-      response.should be_success
-    end
-  end
-
 end
