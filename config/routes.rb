86c6eb61b2b25cb48fdead2fbb599e06eb0cddac
Taylor Cavaletto <tcavalet@asu.edu>
2014-02-16 14:06:45 -0700

    issue #830 completed

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,11 @@ GitMyCurriculum::Application.routes.draw do
 
   get "dashboard/dashboard_main"
 
+  get "users/show"
+ 
+
   devise_for :users 
+  get 'users/:id' => 'users#show', as: :user
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
