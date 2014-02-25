90668b735346deff916937369cedef9d94479c52
Taylor Cavaletto <tcavalet@asu.edu>
2014-02-25 13:05:02 -0700

    solved issue #887

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 # Controller the dashboard features
 class DashboardController < ApplicationController
   def dashboard_main
+    @created_curricula = Curricula.where('creator_id = ?', current_user.id)
+    @contributed_curricula = UserCurricula.joins(:curricula).where('user_id=? AND curriculas.creator_id!=?', current_user.id, current_user.id)
   end
 end
