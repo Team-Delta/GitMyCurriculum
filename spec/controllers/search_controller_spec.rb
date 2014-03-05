503907092aa64dd29f4133e62d4bd2965628c408
Taylor Cavaletto <tcavalet@asu.edu>
2014-03-04 23:07:00 -0700

    some unit tests

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -25,4 +25,34 @@ describe SearchController do
       User.search { keywords 'fl' }.results.should == []
     end
   end
+
+  describe 'user follow', search: true do
+    it 'returns user followed' do
+      user = create(:user)
+      sign_in user
+      #Sunspot.commit
+      #user_search - User.search { keywords 'tc' }
+      #user_results=user_search.results
+      #redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
+      get(:s_follow, {'username' => user.username}, {'query' => 'ba'})
+      assert_redirected_to search_uc_search_path
+      assert_equal 'You are now following Bailey Ammons.', flash[:success]
+    end
+  end
+
+  describe 'user unfollow', search: true do
+    it 'returns user followed' do
+      user = create(:user)
+      sign_in user
+      #Sunspot.commit
+      #user_search - User.search { keywords 'tc' }
+      #user_results=user_search.results
+      #redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
+      get(:s_follow, {'username' => user.username}, {'query' => 'ba'})
+      get(:s_unfollow, {'username' => user.username}, {'query' => 'ba'})
+      assert_redirected_to search_uc_search_path
+      assert_equal 'You are no longer following Bailey Ammons.', flash[:success]
+    end
+  end
+
 end
