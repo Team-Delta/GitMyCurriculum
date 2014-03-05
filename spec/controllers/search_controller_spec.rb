14f996049f7c1104ba3a400fb2cca6dc23afdf91
Taylor Cavaletto <tcavalet@asu.edu>
2014-03-04 23:18:30 -0700

    rubocop fix

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -30,11 +30,11 @@ describe SearchController do
     it 'returns user followed' do
       user = create(:user)
       sign_in user
-      #Sunspot.commit
-      #user_search - User.search { keywords 'tc' }
-      #user_results=user_search.results
-      #redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
-      get(:s_follow, {'username' => user.username}, {'query' => 'ba'})
+      # Sunspot.commit
+      # user_search - User.search { keywords 'tc' }
+      # user_results=user_search.results
+      # redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
+      get(:s_follow, { 'username' => user.username },  'query' => 'ba ')
       assert_redirected_to search_uc_search_path
       assert_equal 'You are now following Bailey Ammons.', flash[:success]
     end
@@ -44,12 +44,12 @@ describe SearchController do
     it 'returns user followed' do
       user = create(:user)
       sign_in user
-      #Sunspot.commit
-      #user_search - User.search { keywords 'tc' }
-      #user_results=user_search.results
-      #redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
-      get(:s_follow, {'username' => user.username}, {'query' => 'ba'})
-      get(:s_unfollow, {'username' => user.username}, {'query' => 'ba'})
+      # Sunspot.commit
+      # user_search - User.search { keywords 'tc' }
+      # user_results=user_search.results
+      # redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
+      get(:s_follow, { 'username' => user.username },  'query' => 'ba ')
+      get(:s_unfollow, { 'username' => user.username },  'query' => 'ba ')
       assert_redirected_to search_uc_search_path
       assert_equal 'You are no longer following Bailey Ammons.', flash[:success]
     end
