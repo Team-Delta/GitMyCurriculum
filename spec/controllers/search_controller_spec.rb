b2d52aa899d8db2aa75b0f63e79a0d3974332f38
Taylor Cavaletto <tcavalet@asu.edu>
2014-03-05 12:00:05 -0700

    search unit test fix

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -30,13 +30,14 @@ describe SearchController do
     it 'returns user followed' do
       user = create(:user)
       sign_in user
+      user_follow = create(:user_follow)
       # Sunspot.commit
       # user_search - User.search { keywords 'tc' }
       # user_results=user_search.results
       # redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
-      get(:s_follow, { 'username' => user.username },  'query' => 'ba ')
+      get(:s_follow, { 'username' => user_follow.username },  'query' => 'ta')
       assert_redirected_to search_uc_search_path
-      assert_equal 'You are now following Bailey Ammons.', flash[:success]
+      assert_equal 'You are now following Taylor Cavaletto.', flash[:success]
     end
   end
 
@@ -44,41 +45,42 @@ describe SearchController do
     it 'returns user followed' do
       user = create(:user)
       sign_in user
+      user_follow = create(:user_follow)
       # Sunspot.commit
       # user_search - User.search { keywords 'tc' }
       # user_results=user_search.results
       # redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
-      get(:s_follow, { 'username' => user.username },  'query' => 'ba ')
-      get(:s_unfollow, { 'username' => user.username },  'query' => 'ba ')
+      get(:s_follow, { 'username' => user_follow.username },  'query' => 'ta')
+      get(:s_unfollow, { 'username' => user_follow.username },  'query' => 'ta')
       assert_redirected_to search_uc_search_path
-      assert_equal 'You are no longer following Bailey Ammons.', flash[:success]
+      assert_equal 'You are no longer following Taylor Cavaletto.', flash[:success]
     end
   end
 
   describe 'user follow failed no current user', search: true do
     it 'returns user followed' do
-      user = create(:user)
+      user_follow = create(:user_follow)
       # Sunspot.commit
       # user_search - User.search { keywords 'tc' }
       # user_results=user_search.results
       # redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
-      get(:s_follow, { 'username' => user.username },  'query' => 'ba ')
+      get(:s_follow, { 'username' => user_follow.username },  'query' => 'ta')
       assert_redirected_to search_uc_search_path
-      assert_equal 'You must login to follow Bailey Ammons.', flash[:error]
+      assert_equal 'You must login to follow Taylor Cavaletto.', flash[:error]
     end
   end
 
   describe 'user unfollow failed no current user', search: true do
     it 'returns user followed' do
-      user = create(:user)
+      user_follow = create(:user_follow)
       # Sunspot.commit
       # user_search - User.search { keywords 'tc' }
       # user_results=user_search.results
       # redirect_to search_s_unfollow_path(:username => u.username, :query => params[:query])
-      get(:s_follow, { 'username' => user.username },  'query' => 'ba ')
-      get(:s_unfollow, { 'username' => user.username },  'query' => 'ba ')
+      get(:s_follow, { 'username' => user_follow.username },  'query' => 'ta')
+      get(:s_unfollow, { 'username' => user_follow.username },  'query' => 'ta')
       assert_redirected_to search_uc_search_path
-      assert_equal 'You must login to unfollow Bailey Ammons.', flash[:error]
+      assert_equal 'You must login to unfollow Taylor Cavaletto.', flash[:error]
     end
   end
 
