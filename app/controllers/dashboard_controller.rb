b5194aeac1482abbf66e0bd34773d8f7e59d1b52
bammons <baileyammons@gmail.com>
2014-03-11 23:56:04 -0700

    closes #49

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,87 @@
 # Controller the dashboard features
 class DashboardController < ApplicationController
   def dashboard_main
-    @created_curricula = Curricula.where('creator_id = ?', current_user.id)
-    @contributed_curricula = UserCurricula.joins(:curricula).where('user_id=? AND curriculas.creator_id!=?', current_user.id, current_user.id)
+    begin
+      @created_curricula = Curricula.where('creator_id = ?', current_user.id)
+      @contributed_curricula = UserCurricula.joins(:curricula).where('user_id=? AND curriculas.creator_id!=?', current_user.id, current_user.id)
+      
+    rescue
+    end
+    check_for_new_commits
+    @notifications = Array.new
+    for c in @created_curricula
+      c.notifications.find_each do |n|
+        @notification = Hash.new
+        if !n.message.nil?
+          set_notification_hash(n.notification_type, n.message)
+        end
+      end
+    end
+
+    for c in @contributed_curricula
+      c.curricula.notifications.find_each do |n|
+        @notification = Hash.new
+        if !n.message.nil?
+          set_notification_hash(n.notification_type, n.message)
+        end
+      end
+    end
+  end
+
+  private
+
+  def check_for_new_commits
+    for @c in @created_curricula
+      path = Rails.root + @c.path
+      @git = Git.bare(path)
+      @log = @git.log
+      for l in @log
+        notification = @c.notifications.where('commit_id = ?', l.sha[0..8]).first
+        if notification == nil
+          create_notification_for l
+        end
+      end
+    end
+
+    for @c in @contributed_curricula
+      path = Rails.root + @c.path
+      @git = Git.bare(path)
+      @log = @git.log
+      for l in @log
+        notification = @c.notifications.where('commit_id = ?', l.sha[0..8]).first
+        if notification == nil
+          create_notification_for l
+        end
+      end
+    end
+  end
+
+  def create_notification_for(commit)
+    @user = User.find_user_by_email commit.author.email
+    @notification = Notification.new()
+    @notification.author = @user
+    @notification.notification_type = 0
+    @notification.message = "#{@user.username} has saved to stream #{@git.branch.name} at #{@c.creator.username}/#{@c.cur_name}.\n#{commit.sha[0..8]} #{commit.message}"
+    @notification.created_at = commit.author.date.strftime("%y-%m-%d")
+    @notification.commit_id = commit.sha[0..8]
+    @c.notifications << @notification
+    @notification.save
+  end
+
+  def set_notification_hash (notif_type, message)
+    @notification[:message] = message
+    case notif_type
+    when 0, 2, 3, 4
+      @notification[:css_class] = 'bg-info text-primary'
+    when 1
+      @notification[:css_class] = 'bg-warning text-warning'
+    when 5
+       @notification[:css_class] = 'bg-success text-success'
+    when 6
+       @notification[:css_class] = 'bg-danger text-danger'
+    else
+       @notification[:css_class] = 'bg-info text-primary'
+    end
+    @notifications.push(@notification)
   end
 end
--- spec/features/create_curriculum_spec.rb
+++ spec/features/create_curriculum_spec.rb
@@ -1,16 +1,31 @@
 require 'spec_helper'
 
-feature 'Edit profile' do
+feature 'CRUD for a curriculum' do
   background do
     create(:user)
     sign_in_with 'bammons123', '12345678'
   end
 
-  scenario'Make a Curriculum' do
+  scenario 'make a Curriculum' do
     make_curriculum_with 'This', 'Is AWESOME'
     expect(page).to have_content('Successfully created curriculum')
   end
 
+  scenario 'open a Curriculum' do
+    make_curriculum_with 'This', 'Is AWESOME'
+    visit authenticated_root_path
+    click_link 'This'
+    expect(page).to have_content('master')
+  end
+
+  scenario 'create files and save them to Curriculum' do
+    make_curriculum_with 'This', 'Is AWESOME'
+
+  end
+
+  def clone_repository
+  end
+  
   def sign_in_with(email, password)
     visit splash_load_path
     fill_in 'email or username', with: email
@@ -19,7 +34,7 @@ feature 'Edit profile' do
   end
 
   def make_curriculum_with(name, description)
-    visit   '/curricula/create'
+    visit create_curriculum_path
     fill_in 'name', with: name
     fill_in 'description', with: description
     click_button 'Create'
