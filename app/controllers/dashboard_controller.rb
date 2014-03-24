da6dfa7501d9afc43ba758aa2849f31e023f3acd
Taylor Cavaletto <tcavalet@asu.edu>
2014-03-24 16:52:53 -0700

    dashboard duplication fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -40,22 +40,22 @@ class DashboardController < ApplicationController
   def check_for_new_commits
     @created_curricula.each do |c|
       @git = get_bare_repo c
-      @log = @git.log
-      @log.each do |l|
-        notification = c.notifications.where('commit_id = ?', l.sha[0..8]).first
-        @user = User.find_user_by_email l.author.email
-        create_notification_for(0, @user, c, @git.branch.to_s, l) if notification.nil?
-      end
+      check_for_subfunction(c, @git)
     end
 
     @contributed_curricula.each do |c|
       @git = get_bare_repo c.curricula
-      @log = @git.log
-      @log.each do |l|
-        notification = c.notifications.where('commit_id = ?', l.sha[0..8]).first
-        @user = User.find_user_by_email l.author.email
-        create_notification_for(0, @user, c, @git.branch.to_s, l) if notification.nil?
-      end
+      check_for_subfunction(c, @git)
+    end
+    
+  end
+
+  def check_for_subfunction(c, git)
+    @log = git.log
+    @log.each do |l|
+      notification = c.notifications.where('commit_id = ?', l.sha[0..8]).first
+      @user = User.find_user_by_email l.author.email
+      create_notification_for(0, @user, c, git.branch.to_s, l) if notification.nil?
     end
   end
 end
