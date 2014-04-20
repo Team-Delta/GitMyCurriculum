157a3103e05b3964025f14d86296b4159801c95d
bammons <baileyammons@gmail.com>
2014-04-19 18:50:53 -0700

    closes #187

--- app/controllers/curricula_controller.rb
+++ app/controllers/curricula_controller.rb
@@ -6,8 +6,9 @@ class CurriculaController < ApplicationController
     @curriculum = Curricula.find_by_id(params[:id])
     @git = ::GitFunctionality::Repo.new.get_bare_repo @curriculum
     @git_working = ::GitFunctionality::Repo.new.get_working_repo @curriculum
+    file_path = "#{Rails.root}/repos/#{@curriculum.creator.username}/#{@curriculum.cur_name}/working/#{@curriculum.cur_name}/#{@curriculum.cur_name}.zip"
+    File.delete(file_path)
     @git_working.pull
-    # @log = @git.log
     @branches = @git.branches
     path_properties = { name: "#{@curriculum.creator.username}", path: profile_load_path(id: @curriculum.creator.username) }
     path_properties2 = { name: "#{@curriculum.cur_name}", path: curricula_path(id: @curriculum.id) }
