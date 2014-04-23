fe837178eb14d9719922e1807ca517495c1fa5c9
Lincoln Stewart <lestewar@asu.edu>
2014-04-22 21:21:16 -0700

    patch 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
-    # flash[:error] = "Get The FuCK OUT"
-    # redirect_to root_url
-    # render :status => 404
-    render file: 'public/404.html'
+  render file: 'public/404.html'
   end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
